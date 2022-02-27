0dd633c448fc66cfac2b473d415558c07f9b3f82
Allan Burns <allanlburns@yahoo.com>
2022-02-27 11:53:42 -0500

    Add boilerplate code and wrote fetchData function to search movie data

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const fetchData =ansync () => {
-    const response = await axios.get('http://www.omdbapi.com');
+const fetchData = async () => {
+    // Using base URL in argument and then some Axios functionality to write the rest
+    // more neatly
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '9692fe4b',
+            i: 'tt0848228'
+        }
+    });
+
+    console.log(response.data);
 };
+
+fetchData();
