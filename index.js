e0222b26fef023073a5367a91050001bb76f7823
Allan Burns <allanlburns@yahoo.com>
2022-02-27 12:30:18 -0500

    Implemented typing delay to send search request

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
     // Using base URL in argument and then some Axios functionality to write the rest
     // more neatly
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '9692fe4b',
-            i: 'tt0848228'
+            s: searchTerm
         }
     });
 
     console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 500);
+};
+
+input.addEventListener('input', onInput);
