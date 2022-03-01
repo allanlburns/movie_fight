276172cbe3ecaba6ff6a0a08dc5923ce5d8bc49c
Allan Burns <allanlburns@yahoo.com>
2022-03-01 18:40:36 -0500

    Create follow up API call to fetch movie details

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const onInput = async event => {
         option.addEventListener('click', () => {
             dropdown.classList.remove('is-active');
             input.value = movie.Title;
+            onMovieSelect(movie);
         });
 
         resultsWrapper.appendChild(option);
@@ -67,3 +68,14 @@ document.addEventListener('click', event => {
         dropdown.classList.remove('is-active');
     }  
 });
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '9692fe4b',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data);
+};
